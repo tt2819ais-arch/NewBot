66afba69e9c45c25c0c15e9dc39e08b13ebf7499
tt2819ais-arch <tt2819ais@gmail.com>
2025-12-24 22:47:31 +0300

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -3,5 +3,5 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 class AgentForm(StatesGroup):
     waiting_for_data = State()
 
-class SessionStates(StatesGroup):
-    setting_target = State()
+class SessionForm(StatesGroup):
+    waiting_for_target = State()
