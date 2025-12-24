9182340f9b0dbdd8ec65222253ba66a3d50da582
tt2819ais-arch <tt2819ais@gmail.com>
2025-12-24 23:05:37 +0300

    Delete bot directory

--- bot/states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class AgentForm(StatesGroup):
-    waiting_for_data = State()
-
-class SessionForm(StatesGroup):
-    waiting_for_target = State()
